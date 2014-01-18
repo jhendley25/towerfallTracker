859e5cf84338734d73a877fb303d21ba3029d633
jhendley25 <jake.hendley25@gmail.com>
2014-01-18 13:10:32 -0500

    added cool data

--- app/scripts/global-data.js
+++ app/scripts/global-data.js
@@ -2,22 +2,29 @@ towerfallDataByUser = {
 
   "user1" : {
     "name" : "jake",
-    "wins" : 112
+    "wins" : 112,
+    "weekly_wins": 33,
+    "daily": 3
   },
   "user2" : {
     "name" : "ari",
-    "wins" : 1
+    "wins" : 42,
+    "weekly_wins": 12,
+    "daily": 5
   },
   "user3" : {
     "name" : "mason",
-    "wins" : 69
+    "wins" : 69,
+    "weekly_wins": 7,
+    "daily": 3
   }
 
 
 }
 
 towerfallDataTruncated = {
-  "jake" : 112,
-  "ari" : 11,
-  "mason" : 169
+  "jake" : [112,33,3],
+  "ari" : [42,12,5],
+  "mason" : [69,7,3]
 }
+
