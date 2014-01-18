758aebe5fc798df54b66b51ca21cd33aeedc672c
jhendley25 <jake.hendley25@gmail.com>
2014-01-18 12:44:16 -0500

    added stubbed out data

---
+++ app/scripts/global-data.js
@@ -0,0 +1,23 @@
+var towerfallDataByUser = {
+
+  "user1" : {
+    "name" : "jake",
+    "wins" : 112
+  },
+  "user2" : {
+    "name" : "ari",
+    "wins" : 1
+  },
+  "user3" : {
+    "name" : "mason",
+    "wins" : 69
+  }
+
+
+}
+
+var towerfallDataTruncated = {
+  "jake" : 112,
+  "ari" : 11,
+  "mason" : 169
+}
